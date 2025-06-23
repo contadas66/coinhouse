d84c59ec35aa4b69df59c2b0c3d2e11aff1fe4cf
contadas66 <contasdas66@gmail.com>
2025-06-22 22:39:18 -0300

    feat: Atualiza favicon do site
    - Altera referência do favicon de coinhouse-logo.png para favicon.ico
    - Adiciona type='image/x-icon' para melhor compatibilidade
    - Prepara estrutura para o novo favicon personalizado

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <link rel="icon" href="/coinhouse-logo.png" />
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
       </head>
       <body>
         <ThemeProvider
